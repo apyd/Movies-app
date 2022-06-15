96d05bdce789160d5fbe488ba8fdd2e5ac7cd5f4
apyd <arturpydych@gmail.com>
2022-06-15 21:40:00 +0200

    Added missing SVG icons, status modal stylings

--- src/components/Modal/Confirmation/StatusModal/StatusModal.tsx
+++ src/components/Modal/Confirmation/StatusModal/StatusModal.tsx
@@ -1,12 +1,13 @@
 import React, { FC, ReactNode } from "react";
 import { Modal } from "../../Modal";
-import Success from "../../../../assets/tick.svg";
-import Error from "../../../../assets/cancelled.svg";
+import Success from "../../../../assets/success.svg";
+import Error from "../../../../assets/error.svg";
 import { IStatusModalProps } from "./StatusModal.types";
+import "./StatusModal.scss";
 
 export const STATUS_ICON: { [key: string]: ReactNode } = {
-  SUCCESS: <Success />,
-  ERROR: <Error />,
+  SUCCESS: <Success className="icon" />,
+  ERROR: <Error className="icon" />,
 };
 
 export const STATUS_TEXT: { [key: string]: string } = {
@@ -15,11 +16,13 @@ export const STATUS_TEXT: { [key: string]: string } = {
 };
 
 export const StatusModal: FC<IStatusModalProps> = ({ status }) => {
+  const Icon = STATUS_ICON[status];
   return (
     <Modal title="" toggleModal={() => {}} isOpened="false">
-      <div className="status-modal"></div>
-      {STATUS_ICON[status]}
-      <p className="status-modal__text">{STATUS_TEXT[status]}</p>
+      <div className="status-modal">
+        {Icon}
+        <p className="status-modal__text">{STATUS_TEXT[status]}</p>
+      </div>
     </Modal>
   );
 };
