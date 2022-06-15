b1f17d8cb73a4bc20fd89567473d7621d1be530c
apyd <arturpydych@gmail.com>
2022-06-15 21:17:35 +0200

    RWD fixes

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -14,16 +14,18 @@ import { ResultsCount } from "./ResultsCount/ResultsCount";
 export const Results = () => {
   return (
     <div className="results">
-      <div className="results__modifiers">
-        <FilterList {...FILTER_DATA} />
-        <Sort
-          options={SORT_OPTIONS}
-          onOptionChange={() => {}}
-          sortLabel="Sort by"
-        />
+      <div className="results__inner-container">
+        <div className="results__modifiers">
+          <FilterList {...FILTER_DATA} />
+          <Sort
+            options={SORT_OPTIONS}
+            onOptionChange={() => {}}
+            sortLabel="Sort by"
+          />
+        </div>
+        <ResultsCount value={RESULTS_COUNT} />
+        <MovieList {...RESULTS} />
       </div>
-      <ResultsCount value={RESULTS_COUNT} />
-      <MovieList {...RESULTS} />
     </div>
   );
 };
