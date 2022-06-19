09f85760f7d98fd04b37cdc6e3a9a1af67287968
apyd <arturpydych@gmail.com>
2022-06-19 18:53:19 +0200

    hooks

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -4,7 +4,6 @@ export const useModal = () => {
   const [isOpened, setIsOpened] = useState(false);
   const toggleModal = () => {
     setIsOpened((isOpened) => !isOpened);
-    console.log(isOpened);
   };
   return [isOpened, toggleModal];
 };
