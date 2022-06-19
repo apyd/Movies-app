18af72627d8a9037a2e9268fa4c55228970bdf7f
apyd <arturpydych@gmail.com>
2022-06-19 18:51:11 +0200

    api data example

--- src/api/resultsData.js
+++ src/api/resultsData.js
@@ -1,55 +1,62 @@
 export const SORT_OPTIONS = [
-    {
-        id: 1,
-        label: "Release date",
-        value: 0,
-        isDefault: true,
-    },
-    {
-        id: 2,
-        label: "Year",
-        value: 1,
-        isDefault: false,
-    },
-    {
-        id: 3,
-        label: "Movie name",
-        value: 2,
-        isDefault: false,
-    },
+  {
+    id: 1,
+    label: "Release date",
+    value: 0,
+    isDefault: true,
+  },
+  {
+    id: 2,
+    label: "Year",
+    value: 1,
+    isDefault: false,
+  },
+  {
+    id: 3,
+    label: "Movie name",
+    value: 2,
+    isDefault: false,
+  },
+];
+
+export const GENRES = [
+  { id: "1", title: "Documentary" },
+  { id: "2", title: "Comedy" },
+  { id: "3", title: "Horror" },
+  { id: "4", title: "Crime" },
 ];
 
 export const FILTER_DATA = [
-    {
-        id: "1",
-        name: "Genre",
-        value: "All",
-        isChecked: true,
-    },
-    {
-        id: "2",
-        name: "Genre",
-        value: "Documentary",
-        isChecked: false,
-    },
-    {
-        id: "3",
-        name: "Genre",
-        value: "Comedy",
-        isChecked: false,
-    },
-    {
-        id: "4",
-        name: "Genre",
-        value: "Horror",
-        isChecked: false,
-    },
-    {
-        id: "5",
-        name: "Genre",
-        value: "Crime",
-        isChecked: false,
-    },
+  {
+    id: "1",
+    name: "Genre",
+    value: "All",
+    isChecked: true,
+  },
+  {
+    id: "2",
+    name: "Genre",
+    value: "Documentary",
+    isChecked: false,
+  },
+  {
+    id: "3",
+    name: "Genre",
+    value: "Comedy",
+    isChecked: false,
+  },
+  {
+    id: "4",
+    name: "Genre",
+    value: "Horror",
+    isChecked: false,
+  },
+  {
+    id: "5",
+    name: "Genre",
+    value: "Crime",
+    isChecked: false,
+  },
 ];
 
 export const RESULTS_COUNT = 39;
