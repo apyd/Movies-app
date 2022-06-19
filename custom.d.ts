f8bc726b91b8ad1430820f08753f54cb44317e42
apyd <arturpydych@gmail.com>
2022-06-19 18:51:51 +0200

    custom.d.ts update

--- custom.d.ts
+++ custom.d.ts
@@ -1,6 +1,7 @@
 declare module '*.svg' {
     import React from 'react';
     const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    export const src: string;
     export default ReactComponent;
 }
 
