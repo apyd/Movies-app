d9ecd06aec047482f9f5805851a3b759e62674f7
apyd <arturpydych@gmail.com>
2022-06-26 19:40:08 +0200

    store and apiSlice

---
+++ src/store/api/apiSlice.ts
@@ -0,0 +1,44 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const BASE_URL = 'https://localhost:4000/';
+
+export const apiSlice = createApi({
+  reducerPath: 'moviesApi',
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  tagTypes: ['Movies'],
+  endpoints: (builder) => ({
+    getMovies: builder.query({
+      query: () => `/movies`,
+      providesTags: ['Movies'],
+    }),
+    getMovieById: builder.query({
+      query: ({ id }) => `/movies/${id}`,
+    }),
+    addMovie: builder.mutation({
+      query: (movie) => ({
+        url: `/movies`,
+        method: 'POST',
+        body: movie,
+      }),
+      invalidatesTags: ['Movies'],
+    }),
+    updateMovieById: builder.mutation({
+      query: (movie) => ({
+        url: `/movies/${movie.id}`,
+        method: 'PUT',
+        body: movie,
+      }),
+      invalidatesTags: ['Movies'],
+    }),
+    deleteMovieById: builder.mutation({
+      query: (id) => ({
+        url: `/movies`,
+        method: 'DELETE',
+        body: { id: id },
+      }),
+      invalidatesTags: ['Movies'],
+    }),
+  }),
+})
+
+export const { useGetMoviesQuery, useGetMovieByIdQuery, useAddMovieMutation, useUpdateMovieByIdMutation, useDeleteMovieByIdMutation } = apiSlice
