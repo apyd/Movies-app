86a8bda27f6d4d08d7d2203e789b5467e06f7503
apyd <arturpydych@gmail.com>
2022-07-04 23:07:09 +0200

    tsconfig, webpack and css.d.ts update - scss modules

--- webpack.config.ts
+++ webpack.config.ts
@@ -37,8 +37,22 @@ const config: Configuration = {
         exclude: [/node_modules/]
       },
       {
-        test: /\.(s[a|c]|c)ss$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: "[name]-[local]__[hash:base64:5]",
+              },
+              sourceMap: true
+            }
+          },
+          'sass-loader'
+        ]
       },
       {
         test: /\.svg$/i,
