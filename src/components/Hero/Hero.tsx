4a0d401ffd26b1b6b9b02cbd35257281b88345c5
apyd <arturpydych@gmail.com>
2022-08-07 11:48:55 +0200

    Hero refactor to support SSR

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,72 +1,61 @@
 import React, { useState } from 'react';
-// import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { useRouter } from 'next/router';
 import classNames from 'classnames/bind';
-// import {
-//   useAddMovieMutation,
-//   useGetMovieByIdQuery,
-// } from "../../store/api/apiSlice";
-// import { useModal } from "../../hooks/useModal";
-// import { AddMovie } from "../Modal/AddMovie/AddMovie";
-// import { SearchView } from "./SearchView/SearchView";
-// import { MovieDetails } from "./MovieDetails/MovieDetails";
+import { useAddMovieMutation } from '../../store/api/apiSlice';
+import { useModal } from '../../hooks/useModal';
+import { AddMovie } from '../Modal/AddMovie/AddMovie';
+import { SearchView } from './SearchView/SearchView';
+import { MovieDetails } from './MovieDetails/MovieDetails';
 import styles from './Hero.module.scss';
 
 const cx = classNames.bind(styles);
 
-export const Hero = () => {
-  // const [addMovie, { isLoading, isError, isSuccess, reset }] =
-  //   useAddMovieMutation();
-  // const [AddMovieModal, toggleModal] = useModal("Add movie", AddMovie, reset);
-  // const navigate = useNavigate();
-  // const { searchQuery } = useParams();
-  // const [params, setQueryParams] = useSearchParams();
-  // const movieId = params.get("movieId");
-  // const { data: movie, isLoading: isDataLoading } =
-  //   useGetMovieByIdQuery(movieId);
+export const Hero = ({ movie }: any) => {
+  const router = useRouter();
+  const { movieId, ...params } = router.query;
+  const { params: searchQuery, ...queryParams } = params;
+  const [addMovie, { isLoading, isError, isSuccess, reset }] = useAddMovieMutation();
+  const [AddMovieModal, toggleModal] = useModal('Add movie', AddMovie, reset);
 
-  // const [query, setQuery] = useState(searchQuery || "");
+  const [query, setQuery] = useState(searchQuery?.toString() || '');
+  console.log(searchQuery);
 
   const onChange = (value: string) => {
-    // setQuery(value);
+    setQuery(value);
   };
 
   const onSearch = (e: React.FormEvent<HTMLElement>) => {
     e.preventDefault();
-  };
-  //   const queryParams =
-  //     "?" + new URLSearchParams(params).toString().replace("filter", "genre");
-  //   navigate(`../search/${query}${queryParams}`);
-  // };
-
-  const switchToSearchView = () => {
-    // const queryParams = { ...Object.fromEntries(params) };
-    // const { movieId, ...filteredQueryParams } = queryParams;
-    // setQueryParams(filteredQueryParams);
+    updateQueryParams();
   };
 
-  //   return (
-  //     <div className={cx(styles.hero, { "hero--search": !movieId })}>
-  //       <div className={cx("hero__inner")}>
-  //         {movieId && !isDataLoading ? (
-  //           <MovieDetails onSearchIconPress={switchToSearchView} {...movie} />
-  //         ) : (
-  //           <SearchView
-  //             toggleModal={toggleModal}
-  //             value={query}
-  //             onChange={onChange}
-  //             onSearch={onSearch}
-  //           />
-  //         )}
-  //       </div>
-  //       <AddMovieModal
-  //         onFormSubmit={addMovie}
-  //         isError={isError}
-  //         isSuccess={isSuccess}
-  //         isLoading={isLoading}
-  //       />
-  //     </div>
-  //   );
-  // };
+  const updateQueryParams = () => {
+    const updatedQueryParams = new URLSearchParams(queryParams)
+      .toString()
+      .replace('filter', 'genre');
+    router.push(`/search/${query}?${updatedQueryParams}`);
+  };
 
-  return <p>Hero</p>;
+  return (
+    <div className={cx(styles.hero, { 'hero--search': !movieId })}>
+      <div className={cx('hero__inner')}>
+        {movieId ? (
+          <MovieDetails onSearchIconPress={updateQueryParams} {...movie} />
+        ) : (
+          <SearchView
+            toggleModal={toggleModal}
+            value={query}
+            onChange={onChange}
+            onSearch={onSearch}
+          />
+        )}
+      </div>
+      <AddMovieModal
+        onFormSubmit={addMovie}
+        isError={isError}
+        isSuccess={isSuccess}
+        isLoading={isLoading}
+      />
+    </div>
+  );
 };
