6d43bed20b3c0d5704befc14e99076940a96ca9e
apyd <arturpydych@gmail.com>
2022-08-16 21:20:45 +0200

    build fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
-import { renderStatic } from '../config/rerender';
 
 export default class AppDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    const { css, ids } = await renderStatic(initialProps.html);
-    return {
-      ...initialProps,
-      styles: (
-        <React.Fragment>
-          {initialProps.styles}
-          <style data-emotion={`css ${ids.join(' ')}`} dangerouslySetInnerHTML={{ __html: css }} />
-        </React.Fragment>
-      )
-    };
-  }
   render() {
     return (
       <Html>
